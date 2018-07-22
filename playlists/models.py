9f45546e25a06b4449cf6c18897e7b0ed01e5565
Cameron Hurd <me@cameronhurd.com>
2018-07-22 14:59:03 -0400

    quick and minor refactor

--- playlists/models.py
+++ playlists/models.py
@@ -3,12 +3,13 @@ from django.db import models
 ARTIST_TYPES = ['Person', 'Group', 'Orchestra', 'Choir', 'Character', 'Other']
 GENDERS      = ['Female', 'Male', 'None']
 
-gender_choices = sorted((item, item) for item in GENDERS)
+gender_choices = sorted((g, g) for g in GENDERS)
+type_choices   = sorted((t, t) for t in ARTIST_TYPES)
 
 class Artist(models.Model):
     name      = models.CharField(max_length=100)
     sort_name = models.CharField(max_length=100)
-    type      = models.CharField(choices=sorted((item, item) for item in ARTIST_TYPES), max_length=100)
+    type      = models.CharField(type_choices, max_length=100)
     gender    = models.CharField(gender_choices, max_length=6, default = 'None')
     area      = models.CharField(blank=True, max_length=100)
     begin     = models.DateTimeField(blank=True)
--- playlists/tests.py
+++ playlists/tests.py
@@ -1,28 +1,25 @@
 from django.test import TestCase
 from .models import Artist
 
+expected_blank_props = (
+    'name',
+    'type',
+    'sort_name',
+    'area',
+    'ipi',
+    'isni',
+    'alias',
+    'mbid',
+)
+
+expected_none_props = ('begin', 'end')
+
 class ArtistModelTest(TestCase):
     def test_model_has_expected_defaults(self):
         artist = Artist()
 
-        expected_blank_props = (
-            'name',
-            'type',
-            'sort_name',
-            'area',
-            'ipi',
-            'isni',
-            'alias',
-            'mbid',
-        )
-
         for blank in expected_blank_props:
             self.assertIs(getattr(artist, blank), '')
-
-        expected_none_props = (
-            'begin',
-            'end'
-        )
 
         for none in expected_none_props:
             self.assertIs(getattr(artist, none), None)
