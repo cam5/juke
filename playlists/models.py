754d95e2114f38dfc38a1729cd9f200f169bd52c
Cameron Hurd <me@cameronhurd.com>
2018-07-22 14:26:28 -0400

    refactor and test basic instantiation

--- playlists/models.py
+++ playlists/models.py
@@ -3,11 +3,13 @@ from django.db import models
 ARTIST_TYPES = ['Person', 'Group', 'Orchestra', 'Choir', 'Character', 'Other']
 GENDERS      = ['Female', 'Male', 'None']
 
+gender_choices = sorted((item, item) for item in GENDERS)
+
 class Artist(models.Model):
     name      = models.CharField(max_length=100)
     sort_name = models.CharField(max_length=100)
     type      = models.CharField(choices=sorted((item, item) for item in ARTIST_TYPES), max_length=100)
-    gender    = models.CharField(choices=sorted((item, item) for item in GENDERS), max_length=6)
+    gender    = models.CharField(gender_choices, max_length=6, default = 'None')
     area      = models.CharField(blank=True, max_length=100)
     begin     = models.DateTimeField(blank=True)
     end       = models.DateTimeField(blank=True)
