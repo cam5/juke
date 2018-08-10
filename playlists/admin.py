b344a26f22cc2ba99408806dff6deda9836111f3
Cameron Hurd <me@cameronhurd.com>
2018-08-10 18:15:09 -0400

    updates the way that Tracks display in admin

--- playlists/admin.py
+++ playlists/admin.py
@@ -2,7 +2,13 @@
 from django.contrib import admin
 from .models import Track, Artist, Release
 
-# Register your models here.
-admin.site.register(Track)
+
+@admin.register(Track)
+class TrackAdmin(admin.ModelAdmin):
+    """"Special class to layout rules for Track in admin"""
+    list_display = ('number', 'name', 'length', 'release')
+    ordering = ('number',)
+
+
 admin.site.register(Artist)
 admin.site.register(Release)
